bfdd2526e3f6646b0466dfda2c29ace9baca37f5
muhammedfayisthangal@gmail.com <muhammedfayisthangal@gmail.com>
2024-02-24 10:19:29 -0500

    cors err

--- index.js
+++ index.js
@@ -14,13 +14,22 @@ import "dotenv/config";
 const app = express();
 
 // const io = new Server(server, {
-    // cors: {
-        // origin:[ "http://localhost:5173", "https://muhammedfayistgl.github.io",'https://instagram-server-jyei.onrender.com/'],
-        // credentials: true,
-    // },
+// cors: {
+// origin:[ "http://localhost:5173", "https://muhammedfayistgl.github.io",'https://instagram-server-jyei.onrender.com/'],
+// credentials: true,
+// },
 // });
 // 
-app.use(cors({ credentials: true, origin: ["http://localhost:5173", "https://muhammedfayistgl.github.io",'https://instagram-server-jyei.onrender.com/'] }));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+});
+
+app.use(cors({ credentials: true, origin: ["http://localhost:5173", "https://muhammedfayistgl.github.io"] }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,33 +39,33 @@ app.get("/", (req, res) => {
     res.send("Hello, TypeScript Express!");
 });
 // io.on("connection", () => {
-    console.log("New client connected");
-    // socket.on("send-message", async (data) => {
-    //     const { senderId, receiverId, content, roomId } = data;
-    //     socket.join(roomId);
-    //     const isChat = await Chat.aggregate([
-    //         {
-    //             $match: {
-    //                 $and: [
-    //                     {
-    //                         senderId: {
-    //                             $eq: senderId,
-    //                         },
-    //                     },
-    //                     {
-    //                         receiverId: {
-    //                             $eq: receiverId,
-    //                         },
-    //                     },
-    //                 ],
-    //             },
-    //         },
-    //     ]);
+console.log("New client connected");
+// socket.on("send-message", async (data) => {
+//     const { senderId, receiverId, content, roomId } = data;
+//     socket.join(roomId);
+//     const isChat = await Chat.aggregate([
+//         {
+//             $match: {
+//                 $and: [
+//                     {
+//                         senderId: {
+//                             $eq: senderId,
+//                         },
+//                     },
+//                     {
+//                         receiverId: {
+//                             $eq: receiverId,
+//                         },
+//                     },
+//                 ],
+//             },
+//         },
+//     ]);
 
-    //     console.log(data);
+//     console.log(data);
 
-    //     socket.to(isChat[0].roomId).emit("reply-message", content);
-    // });
+//     socket.to(isChat[0].roomId).emit("reply-message", content);
+// });
 // });
 //  Routs
 app.use("/v1", Route);
